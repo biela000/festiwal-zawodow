f8bae9585e55facb60d0e3775b305b85d7226d6f
Jakub Biela <jakub.biela.jb@gmail.com>
2024-04-23 22:28:44 +0200

    add projects to ShowcaseProjects style and display gallery of projects

--- src/data/ShowcaseProjects.ts
+++ src/data/ShowcaseProjects.ts
@@ -1,8 +1,20 @@
 export default [
     {
         title: 'Chińczyk',
-        url: 'chinczyk.pl',
+        url: 'http://dygsow.ct8.pl',
         imgSrc: '/chinczyk.png',
         imgSrcActive: '/chinczyk_active.gif',
+    },
+    {
+        title: 'Bomberman',
+        url: 'http://d3bomb.ct8.pl',
+        imgSrc: '/bomberman.png',
+        imgSrcActive: '/bomberman_active.gif',
+    },
+    {
+        title: 'Saper',
+        url: 'https://biela000.github.io/minesweeper',
+        imgSrc: '/minesweeper.png',
+        imgSrcActive: '/minesweeper_active.gif',
     }
 ];
