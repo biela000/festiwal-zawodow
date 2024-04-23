fe162f34de428b41b7183af7e56249cb4ee8fa2a
Jakub Biela <jakub.biela.jb@gmail.com>
2024-04-23 21:52:38 +0200

    export glowing border animation to a UI component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import Logo from '../Logo/Logo.tsx';
 import styles from './Header.module.css';
+import GlowingBorderCard from '../GlowingBorderCard/GlowingBorderCard.tsx';
 
 export default function Header() {
     return (
-        <header className={styles.header}>
+        <GlowingBorderCard additionalStyles={styles.header}>
             <Logo src='/zsl.png' alt='ZSŁ' />
             <Logo src='/malopolska_zawodowa.png' alt='Małopolska Zawodowa' />
-        </header>
+        </GlowingBorderCard>
     );
 };
